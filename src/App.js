75e4482ae012c8263f03c4904d9081628dd5e66f
Henry KIM <seedtree.henry@gmail.com>
2018-11-01 03:40:19 +1100

    react router - two pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import { ApolloProvider } from "react-apollo";
 import client from "./apolloClient";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Detail from "./Detail";
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
+        <Router>
+          <Switch>
+            <Route exact={true} path={"/"} component={Home} />
+            <Route path={"/details/:countryId"} component={Detail} />
+          </Switch>
+        </Router>
         <div className="app" />
       </ApolloProvider>
     );
